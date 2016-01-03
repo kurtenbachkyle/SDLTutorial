a43ffb5e69fd780f39511c36df1e8a2653ce5929
Kyle Kurtenbach <kurtenbachkyle@users.noreply.github.com>
2016-01-03 16:56:52 -0600

    lesson one completed

--- main.cpp
+++ main.cpp
@@ -13,11 +13,53 @@ int main(int, char**){
 
   // std::cout << "resource path: " << getResourcePath() << std::endl;
 
-  SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 100, 100, SDL_WINDOW_SHOWN);
+  SDL_Window *win = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
   if(win == nullptr){
     std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    return 1;
   }
 
+  SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if(ren == nullptr){
+          SDL_DestroyWindow(win);
+          std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+          SDL_Quit();
+          return 1;
+  }
+
+  std::string imagePath = getResourcePath("lesson1") + "hello.bmp";
+  SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
+
+  if(bmp == nullptr){
+          SDL_DestroyRenderer(ren);
+          SDL_DestroyWindow(win);
+          std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+          SDL_Quit();
+          return 1;
+  }
+
+  SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
+  SDL_FreeSurface(bmp);
+
+  if(tex == nullptr){
+          SDL_DestroyRenderer(ren);
+          SDL_DestroyWindow(win);
+          std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+          SDL_Quit();
+          return 1;
+  }
+
+  SDL_RenderClear(ren);
+  SDL_RenderCopy(ren, tex, NULL, NULL);
+  SDL_RenderPresent(ren);
+
+  SDL_Delay(3000);
+
+  //Destroying things on quit
+  SDL_DestroyTexture(tex);
+  SDL_DestroyRenderer(ren);
+  SDL_DestroyWindow(win);
   SDL_Quit();
   return 0;
 }
